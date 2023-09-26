6d7a7258fb9531a603179cdef88ece25ca58a89a
Paul-Macz <paul.maczkowiak@epfedu.fr>
2023-09-26 17:09:05 +0200

    TP1- Exercices 1 et 2

---
+++ TP1_MACZKOWIAK/TP1_convertisseur_MACZKOWIAK/src/tp1_convertisseur_maczkowiak/TP1_convertisseur_MACZKOWIAK.java
@@ -0,0 +1,78 @@
+/*
+* Nom: Paul Maczkowiak
+* Groupe: TDC
+* TP1 Exercice 2
+* Date: 26/09/23
+ */
+package tp1_convertisseur_maczkowiak;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lukha
+ */
+public class TP1_convertisseur_MACZKOWIAK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double val;
+        int op;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer une temperature reelle :");
+        val = sc.nextDouble();
+        System.out.println("""
+                           Saisissez la conversion que vous souhaiter effectuer :
+                           1)De Celcius vers Kelvin
+                           2)De Kelvin vers Celcius
+                           3)De Farenheit vers Celcius
+                           4)De Celcius vers Farenheit
+                           5)De Kelvin vers Farenheit
+                           6)De Farenheit vers Kelvin
+                           """);
+        op=sc.nextInt();
+        if (op==1){
+            System.out.println(val+" degré Celcius est égal à"+CelciusVersKelvin(val)+" Kelvin");
+        }
+        else if (op==2){
+            System.out.println(val+" Kelvin est égal à"+KelvinVersCelcius(val)+" degré Celcius");
+        }
+        else if (op==3){
+            System.out.println(val+" degré Farenheit est égal à"+FarenheitVersCelcius(val)+" degré Celcius");
+        }
+        else if (op==4){
+            System.out.println(val+" degré Celcius est égal à"+CelciusVersFarenheit(val)+" degré Farenheit");
+        }
+        else if (op==5){
+            System.out.println(val+" Kelvin est égal à"+KelvinVersFarenheit(val)+" degré Farenheit");
+        }
+        else if (op==6){
+            System.out.println(val+" degré Farenheit est égal à"+FarenheitVersKelvin(val)+" Kelvin");
+        }
+        else{
+            System.out.println("Veuillez chosir un opérateur valide");
+        }
+    }
+    
+    public static double CelciusVersKelvin(double tCelcius){
+        return (tCelcius+273.15);
+    } 
+    
+    public static double KelvinVersCelcius(double tKelvin){
+        return (tKelvin-273.15);
+    }
+    public static double FarenheitVersCelcius(double tFarenheit){
+        return ((5.0/9)*(tFarenheit-32));
+    }
+    public static double CelciusVersFarenheit(double tCelcius){
+        return((1.8*tCelcius)+32);
+    }
+    public static double KelvinVersFarenheit(double tKelvin){
+        return (CelciusVersFarenheit(FarenheitVersCelcius(tKelvin)));
+    }
+    public static double FarenheitVersKelvin(double tFarenheit){
+        return (FarenheitVersCelcius(CelciusVersKelvin(tFarenheit)));
+    }
+}
---
+++ TP1_MACZKOWIAK/TP1_manipNombresInt_MACZKOWIAK/src/tp1_manipnombresint_maczkowiak/TP1_manipNombresInt_MACZKOWIAK.java
@@ -0,0 +1,34 @@
+/*
+* Nom: Paul Maczkowiak
+* Groupe: TDC
+* TP1 Exercice 1
+* Date: 26/09/23
+ */
+package tp1_manipnombresint_maczkowiak;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lukha
+ */
+public class TP1_manipNombresInt_MACZKOWIAK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* Variables*/
+        int nb1,nb2;
+        
+        /*Programme*/
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer un nombre :");
+        nb1 = sc.nextInt();
+        System.out.println("\n Entrer un autre nombre :");
+        nb2 = sc.nextInt();
+        System.out.println("Voici differentes interactions entre les nombres "+nb1+" et "+nb2+": \n Somme : "+ (nb1+nb2) +"\n Difference : "+(nb1-nb2)+"\n Produit : "+(nb1*nb2)+"\n Quotient entier : "+(nb1/nb2)+"\n Reste par la division euclidienne : "+(nb1%nb2));
+        
+    }
+
+}
